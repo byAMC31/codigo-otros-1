fcdcf24317737ef8c063d9d54065dcbfe92669c2
Adrian Martinez Calderon <adrianmtzcl@gmail.com>
2025-01-23 11:07:21 -0600

    fix: Se corregieron errores en el codigo para que funcione

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,37 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//Corrección de selectores del DOM
+//Cambiado a para seleccionar por clase
+const $n = document.querySelector('.name'); 
+const $b = document.querySelector('.blog'); 
+const $l = document.querySelector('.location'); 
+
+// Agregamos la palabra clave "async"
+async function displayUser(username) {
+  try {
+     $n.textContent = 'Cargando...'; $b.textContent = ''; $l.textContent = '';
+
+    //Realizamos la solicitud a la API
+    const response = await fetch(`${usersEndpoint}/${username}`);
+
+    //Convertimos la respuesta a JSON
+    const data = await response.json();
+
+    // Mostramos los datos obtenidos en el DOM
+    $n.textContent = data.name 
+    $b.textContent = data.blog 
+    $l.textContent = data.location 
+  } catch (error) {
+    // Manejo de errores en caso de que falle la solicitud o el proceso
+    handleError(error);
+  }
 }
 
+//Función para manejar errores
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.error('Ocurrió un error:', err);
+  $n.textContent = `Algo salió mal: ${err.message}`;$b.textContent = '';$l.textContent = '';
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
